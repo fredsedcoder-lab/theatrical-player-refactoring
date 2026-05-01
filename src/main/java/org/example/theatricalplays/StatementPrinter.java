4a3c15812ece41e2432c6e1519afe3961b9c5ba6
fred <fredsedcoder@gmail.com>
2026-05-01 22:05:43 +0200

    move method amountFor to Performance class

--- src/main/java/org/example/theatricalplays/StatementPrinter.java
+++ src/main/java/org/example/theatricalplays/StatementPrinter.java
@@ -15,41 +15,18 @@ public class StatementPrinter {
 
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
-
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
             if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(amountFor(perf, play) / 100), perf.audience);
-            totalAmount += amountFor(perf, play);
+            result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(perf.amountFor(play) / 100), perf.audience);
+            totalAmount += perf.amountFor(play);
         }
         result += String.format("Amount owed is %s%n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits%n", volumeCredits);
         return result;
-    }
-
-    int amountFor(Performance perf, Play play) {
-        int thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error("unknown type: %s".formatted(play.type));
-        }
-        return thisAmount;
     }
 
 }
