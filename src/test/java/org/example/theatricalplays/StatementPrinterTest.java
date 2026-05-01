09c3d009605c0a42b24916010e37b7c1b29939b7
fred <fredsedcoder@gmail.com>
2026-05-01 22:27:08 +0200

    add aproved tests

--- src/test/java/org/example/theatricalplays/StatementPrinterTest.java
+++ src/test/java/org/example/theatricalplays/StatementPrinterTest.java
@@ -25,4 +25,38 @@ public class StatementPrinterTest {
 
         verify(result);
     }
+    @Test
+    void allComedyPrintTest(){
+        var plays = Map.of(
+                "madame", new Play("Madame", "comedy"),
+                "as-like", new Play("As You Like It", "comedy"),
+                "scapin", new Play("Scapin", "comedy")
+        );
+        var invoice = new Invoice("BigCo", java.util.List.of(
+                new Performance("madame", 4),
+                new Performance("as-like", 15),
+                new Performance("scapin", 40)
+        ));
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.print(invoice, plays);
+
+        verify(result);
+    }
+    @Test
+    void allTragedyPrintTest(){
+        var plays = Map.of(
+                "hamlet", new Play("Hamlet", "tragedy"),
+                "misanthrope", new Play("Misanthrope", "tragedy"),
+                "othello", new Play("Othello", "tragedy")
+        );
+        var invoice = new Invoice("BigCo", java.util.List.of(
+                new Performance("hamlet", 4),
+                new Performance("misanthrope", 30),
+                new Performance("othello", 40)
+        ));
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.print(invoice, plays);
+
+        verify(result);
+    }
 }
