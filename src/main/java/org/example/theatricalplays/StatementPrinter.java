4efb0ce681a67ec0fa66bcb45e679cdc5bc02abb
fred <fredsedcoder@gmail.com>
2026-05-01 21:52:20 +0200

    replace temp variable by query

--- src/main/java/org/example/theatricalplays/StatementPrinter.java
+++ src/main/java/org/example/theatricalplays/StatementPrinter.java
@@ -16,16 +16,14 @@ public class StatementPrinter {
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
 
-            var thisAmount = amountFor(perf, play);
-
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
             if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(thisAmount / 100), perf.audience);
-            totalAmount += thisAmount;
+            result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(amountFor(perf, play) / 100), perf.audience);
+            totalAmount += amountFor(perf, play);
         }
         result += String.format("Amount owed is %s%n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits%n", volumeCredits);
